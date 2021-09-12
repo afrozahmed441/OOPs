69317545bcca65db25b1978c37cdc4a6180d9dff
Afroz <afrozahmed441@gmail.com>
2021-09-12 15:08:36 +0530

    User Class Example

--- c++/intro-classes-and-objects/class.h
+++ c++/intro-classes-and-objects/class.h
@@ -1,6 +1,7 @@
 #ifndef _USER_H_
 #define _USER_H_
 
+#include <iostream>
 #include <string>
 using namespace std;
 
@@ -15,14 +16,43 @@ public:
     User(string name, int userAge, char userGender, string userInfo) {
 
         /// assigning the class properties
-        userName = name;
-        age = userAge;
-        gender = userGender;
-        info = userInfo;
+        setUserName(name);
+        setUserAge(userAge);
+        setUserGender(userGender);
+        setUserInfo(userInfo);
 
     }
 
-        
+    void setUserName(string name) {
+        this->userName = name;
+    }
+
+    void setUserAge(int age) {
+        /// if age is less than or equal to zero then zero 
+        if(age <= 0) this->age = 0;
+        /// else 
+        else this->age = age;
+    }
+
+    void setUserGender(char g) {
+        /// if gender is one of the M || F || O set the user gender to it
+        if(g == 'M' || g == 'F' || g == 'O') this->gender = g;
+        /// else if gender is one of the m || f || o set the user gender to it
+        else if(g == 'm' || g == 'f' || g == 'o') this->gender = g;
+        /// else N (None)
+        else this->gender = 'N';
+    }
+
+    void setUserInfo(string info) {
+        this->info = info;
+    }
+
+    void display() {
+        cout << "Name: " << this->userName << endl;
+        cout << "Age: " << this->age << endl;
+        cout << "Gender: " << this->gender << endl;
+        cout << "Info: "  << this->info << endl;
+    }    
 
 };
 

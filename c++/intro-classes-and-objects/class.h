c724f6f57dc6bc26059af79bc004fd2db3f2b499
Afroz <afrozahmed441@gmail.com>
2021-09-13 18:07:10 +0530

    Example Class User

--- c++/intro-classes-and-objects/class.h
+++ c++/intro-classes-and-objects/class.h
@@ -23,17 +23,19 @@ public:
 
     }
 
+private:
+    /// set User name
     void setUserName(string name) {
         this->userName = name;
     }
-
+    /// set User age
     void setUserAge(int age) {
         /// if age is less than or equal to zero then zero 
         if(age <= 0) this->age = 0;
         /// else 
         else this->age = age;
     }
-
+    /// set User gender
     void setUserGender(char g) {
         /// if gender is one of the M || F || O set the user gender to it
         if(g == 'M' || g == 'F' || g == 'O') this->gender = g;
@@ -42,11 +44,21 @@ public:
         /// else N (None)
         else this->gender = 'N';
     }
-
+    /// set User info
     void setUserInfo(string info) {
         this->info = info;
     }
-
+public:
+    /// return the userName
+    string getUserName(){ return (this->userName); }
+    /// return the age of the user
+    int getUserAge(){ return (this->age); }
+    /// return the gender of the user    
+    char getUserGender() { return (this->gender); }
+    /// return the info of the user
+    string getUserInfo() { return (this->info); }
+    
+    /// display user details
     void display() {
         cout << "Name: " << this->userName << endl;
         cout << "Age: " << this->age << endl;

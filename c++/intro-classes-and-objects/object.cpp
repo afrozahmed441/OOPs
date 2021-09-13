c29864ea33b8cbd9541468d939139c2ac21d6e70
Afroz <afrozahmed441@gmail.com>
2021-09-13 17:52:47 +0530

    Object code

--- c++/intro-classes-and-objects/object.cpp
+++ c++/intro-classes-and-objects/object.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 int main(){
     int size = 3;
+    /// using pointers in creating objects
+    /// array of User pointers
     User *users[size];
 
     users[0] = new User("Afroz Ahmed Shaik", 21, 'M', "hi i am Afroz");
@@ -11,7 +13,16 @@ int main(){
     users[2] = new User("B", 41, 'M', "" );
 
     for(int i = 0; i < size; i++)
+        /// accessing member funtions (with pointers)
         users[i]->display();
     
+    /// without pointers creating objects
+    User afroz("Shaik Afroz Ahmed", 21, 'M', "Hello i am CompSci Student");
+    User adin("Adin Lebon", 20, 'M', "hey there!");
+
+    /// accessing member functions (without pointers)
+    afroz.display();
+    adin.display();
+
  return 0;
 }

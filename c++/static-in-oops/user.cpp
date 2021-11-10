b55f3ff05945c7b0271f4ba6a0e9ca7bff730ee0
Afroz <afrozahmed441@gmail.com>
2021-11-10 08:54:15 +0530

    static data members and member functions

--- c++/static-in-oops/user.cpp
+++ c++/static-in-oops/user.cpp
@@ -6,15 +6,20 @@ using namespace std;
 User::User(int id, string name) {
     this->id = id;
     this->name = name;
+    /// increase the count of users, when objects are created
     ++numberOfUsers;
 }
 
+//// initalizing static data member with the class name and scope resolution operator
 int User::numberOfUsers = 0;
 
+/// static member function can only access static data members 
 void User::printNumberOfUsers() {
     cout << numberOfUsers << endl;
 }
 
+/// destructor 
 User::~User() {
+    /// decrease the count of users, when objects are destroyed.
     numberOfUsers--;
 }
